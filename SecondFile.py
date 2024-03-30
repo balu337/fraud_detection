96aae07e1bbe5d0fe4bd47e4770fb0d89a49d219
balu337 <muralidharg337@gmail.com>
2024-03-30 20:12:56 +0530

    clean slate

--- SecondFile.py
+++ SecondFile.py
@@ -4,20 +4,3 @@ print('Mty second file')
 
 
 
-for i in range ( 0,6):
-<<<<<<< HEAD
-    print(f"hello {i}th time")
-
-#add function 
-
-def add(x,y):
-    return x+y
-
-
-def subtract(x,y):
-    return x-=y
-
-def product(x,y):
-    return x*y
-def div(x,y):
-    return x/y
